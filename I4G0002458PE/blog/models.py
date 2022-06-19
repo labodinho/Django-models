4d0166f94eed24fe35c596863dbbbd85e2e2699b
zechariah popoola <olabode.oritogun@gmail.com>
2022-06-19 05:53:34 +0100

    new changes

--- I4G0002458PE/blog/models.py
+++ I4G0002458PE/blog/models.py
@@ -1,11 +1,10 @@
-from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=20)
     texy = models.TextField()
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField()
     publish_date = models.DateTimeField()
